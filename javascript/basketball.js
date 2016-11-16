8d3ba7b8232d27fe5b976a1fba513db71b16d7ed
codycoolwaffle <codyhaslam@gmail.com>
2016-11-15 23:31:37 -0800

    Week 13

---
+++ javascript/basketball.js
@@ -0,0 +1,68 @@
+//Declaring variables
+var moveBall = [document.getElementById('bb1'), document.getElementById('bb2')];
+var gradientBall = [document.getElementById('gradientBall'), document.getElementById('gradientBall2')];
+var x = [0, 300];
+var rotation = [0, 0];
+var velocityX = [5, 3];
+var y = [0, 400];
+var velocityY = [3, 5];
+var velocity = [Math.sqrt((velocityX[0]*velocityX[0])+(velocityY[0]*velocityY[0])), Math.sqrt((velocityX[1]*velocityX[1])+(velocityY[1]*velocityY[1]))];
+var rotVelocity = [velocity[0] * 360/314, velocity[1] * 360/314];
+var speed = 1;
+
+//Calling main loop
+animate();
+
+//Declaring main loop function
+function animate()
+{
+	for(var i = 0; i < 2; i++)
+	{
+		//Testing if balls are touching edge
+		if(x[i] >= 650 || x[i]<= -50)
+		{
+			//Making balls go backwards
+			velocityX[i] = -velocityX[i];
+			rotVelocity[i] = -rotVelocity[i];
+		}
+		if(y[i] >= 450 || y[i]<= -50)
+		{
+			//Making balls go backwards
+			velocityY[i] = -velocityY[i];
+			rotVelocity[i] = -rotVelocity[i];
+		}
+
+		//Increasing the movement variables
+		x[i] += velocityX[i]*speed;
+		y[i] += velocityY[i]*speed;
+		rotation[i] += rotVelocity[i];
+
+		//Moving and rotating the balls
+		moveBall[i].setAttribute('transform', 'translate(' + x[i] + ' ' + y[i] + ') rotate(' + rotation[i] + ' 100 100)');
+		gradientBall[i].setAttribute('transform', 'translate(' + x[i] + ' ' + y[i] + ')');
+	}
+	//Telling Javascript how much milliseconds to wait before repeating loop
+	setTimeout(animate, 16.6666666667);
+}
+
+//Declaring accelerate button function
+document.getElementById('faster').onclick = function(e)
+{
+	speed *= 0.75;
+}
+
+//Declaring decelerate button function
+document.getElementById('slower').onclick = function(e)
+{
+	speed *= 1.25;
+}
+
+//Function for making balls bounce off each other
+function ballBounce()
+{
+
+	//var square = document.getElementById('square');
+	//var x = (5.83095189485*Math.cos((toRadians(0))+(2*Math.PI)));
+	//var y = (-5.83095189485*Math.sin((toRadians(0))+(2*Math.PI)));
+	//square.setAttribute('transform', 'translate(' + x + ' ' + y + ')');
+}
--- javascript/wafflib.js
+++ javascript/wafflib.js
@@ -1,16 +1,34 @@
+var keys = [];
+
+
 function isOffScreen(entity, w, h)
 {
+	var out = false;
 	r = entity.getAttribute("r")-0;
 
 	if(entity.getAttribute("cx") <= r)
+	{
 		entity.setAttribute("cx", r);
+		out = true;
+	}
 	else if(entity.getAttribute("cx") >= w-r)
+	{
 		entity.setAttribute("cx", w-r);
+		out = true;
+	}
 
 	if(entity.getAttribute("cy") <= r)
+	{
 		entity.setAttribute("cy", r);
+		out = true;
+	}
 	else if(entity.getAttribute("cy") >= h-r)
+	{
 		entity.setAttribute("cy", h-r);
+		out = true;
+	}
+
+	return out;
 }
 
 function alart(thing)
@@ -40,4 +58,95 @@ function shuffle(array)
     }
 
     return array;
+}
+
+function toRadians(degs)
+{
+	return Math.PI*degs/180;
+}
+
+function keynitiate(tag, fps)
+{
+	get(tag)[0].onkeydown = function(event)
+	{
+		keys[event.keyCode] = true;
+	}
+
+	get(tag)[0].onkeyup = function(event)
+	{
+    	keys[event.keyCode] = false;
+    }
+
+    setTimeout(keynitiate(tag, fps), fps);
+}
+
+function returnKey(key)
+{
+	return keys[key];
+}
+
+function print()
+{
+	var g = arguments[0];
+	for (var i = 1; i < arguments.length; i++)
+	{
+		g = g.replace("%s", arguments[i]);
+	}
+	return g;
+}
+
+var logElement;
+function setupLog(element)
+{
+	logElement = element;
+}
+function log(s, bool)
+{
+	if(logElement == null)
+	{
+		console.log(s);
+		return;
+	}
+	if(bool === true)
+		logElement.innerHTML += s + "\n";
+	else
+		logElement.innerHTML += s;
+}
+function clearLog()
+{
+	logElement = null;
+}
+
+var logOut = "";
+function buildLog(s, bool)
+{
+	if(bool === true)
+		logOut += s + "\n";
+	else
+		logOut += s;
+}
+
+function printLog(bool)
+{
+	log(logOut, bool);
+	logOut = "";
+}
+
+function makeTable(content, width, length)
+{
+	buildLog("<table align='center'>", true);
+	for(var y = 0; y < length; y++)
+	{
+		buildLog("<tr>", true);
+		for(var x = 0; x < width; x++)
+		{
+			if(content[(y*width)+x] != null)
+				buildLog("<td>" + content[(y*width)+x] + "</td>", true);
+			else
+				buildLog("<td></td>", true);
+		}
+		buildLog("</tr>", true);
+	}
+	buildLog("</table>", true);
+	printLog(true);
 }
